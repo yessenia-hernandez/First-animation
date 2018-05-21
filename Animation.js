d511556b9fbae6b7c741f8c554610b02938a1759
Yessenia Hernandez Hernandez <yh90926@srcs.loc>
2018-05-21 11:52:31 -0700

    interactive animation finished!

--- Animation.js
+++ Animation.js
@@ -1,4 +1,5 @@
 var box = document.getElementById("animate2");
+var boxx = document.getElementById("animate1");
 var xpos = 0;
 var ypos = 0;
 var xstep = 1;
@@ -12,3 +13,12 @@ function frame() {
     xpos = xpos + xstep;
     box.style.left = xpos + 'px';
 }
+window.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft"){
+        boxx.style.background = "lightpink";
+    } else if (event.key === "ArrowRight") {
+         boxx.style.background = "PaleGreen";
+    } else {
+         boxx.style.background = "rebeccapurple";
+    }
+});
